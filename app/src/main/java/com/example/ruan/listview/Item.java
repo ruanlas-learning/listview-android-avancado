575f0adf0dc2d793393b903adcf5de2a7ef5bb9e
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-15 19:11:39 -0200

    aula 07

---
+++ app/src/main/java/com/example/ruan/listview/Item.java
@@ -0,0 +1,15 @@
+package com.example.ruan.listview;
+
+/*
+* Esta classe representará cada item do ListView
+* */
+public class Item {
+    public int icon;
+    public String title;
+
+    public Item(){
+        super();
+        this.icon = icon;
+        this.title = title;
+    }
+}
---
+++ app/src/main/java/com/example/ruan/listview/ItensAdapter.java
@@ -0,0 +1,13 @@
+package com.example.ruan.listview;
+
+import android.widget.ArrayAdapter;
+
+/*
+* Quando trabalhamos com componentes do tipo listView, os dados que serão exibidos na listagem
+* deste componente devem pertencer a uma coleção do tipo ArrayAdapter. Quando queremos que os dados
+* sejam exibidos de forma customizada, devemos criar um ArrayAdapter personalizado para passar
+* para o listView da forma que for mais conveniente.
+* */
+public class ItensAdapter extends ArrayAdapter<Item> {
+
+}
