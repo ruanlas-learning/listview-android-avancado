dfa874258a81e5a5649ba6a4d5b3b258677853f0
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-15 17:35:31 -0200

    aula 03

--- app/src/main/java/com/example/ruan/listview/MainActivity.java
+++ app/src/main/java/com/example/ruan/listview/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.ruan.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
     }
 }
