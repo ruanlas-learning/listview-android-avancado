0758ea9d07376d0be0f7f5f3d41782d4a72a55b2
Ruan Silva <ruanluiz.as@gmail.com>
2018-11-15 22:58:49 -0200

    aula 14

--- app/src/main/java/com/example/ruan/listview/MainActivity.java
+++ app/src/main/java/com/example/ruan/listview/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.ruan.listview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +33,14 @@ public class MainActivity extends AppCompatActivity {
 
         listView.addHeaderView(header);
         listView.setAdapter(itensAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView v = (TextView)view.findViewById(R.id.txt_nome);
+
+                Toast.makeText(getApplicationContext(), v.getText().toString(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
